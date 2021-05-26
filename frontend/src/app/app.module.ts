0dcc24392f2570fff8e839a3bc207e723d1363df
Dhiego Gomes <dhiego.jw@gmail.com>
2021-05-26 16:42:20 -0300

    :rocket: Adiciona /views e /directives

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,15 +9,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 import { MatSidenavModule  } from '@angular/material/sidenav'
-import { MatListModule  } from '@angular/material/list'
-
+import { MatListModule  } from '@angular/material/list';
+import { MatCardModule  } from '@angular/material/card';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
@@ -25,7 +33,8 @@ import { MatListModule  } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
